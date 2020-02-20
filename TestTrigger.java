48530be0e0ec65faed70769f5a77c04e9f31d5da
Thai Nguyen <ntthaibk@gmail.com>
2020-02-20 03:15:01 -0500

    First commit
    haha

---
+++ TestTrigger.java
@@ -0,0 +1,18 @@
+public class TestTrigger{
+	public static void main(String[] args) {
+		private String firstParam;
+		private String secondParam;
+
+
+		public String getFirstParam(){
+			return firstParam;
+		}
+
+		public void setFirstParam(String firstParam){
+			this.firstParam = firstParam;
+		}
+
+
+
+	}
+}
